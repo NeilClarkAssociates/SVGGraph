0782b0f98fe47fdf96775b43655e7b494bbd9828
David J Holland <stooge213@hotmail.com>
2017-11-23 16:25:45 +1100

    Update example

--- examples/index.php
+++ examples/index.php
@@ -1,10 +1,12 @@
 <?php
 require_once('../SVGGraph.php');
 
-$settings = array();
+$settings = array(
+  'break_on_null' => true
+);
 
 $values = array(
-  array('Dough' => 30, 'Ray' => 50, 'Me' => 40, 'So' => 25, 'Far' => 45, 'Lard' => 35)
+  array('Dough' => 30, 'Ray' => 50, 'Me' => NULL, 'So' => 25, 'Far' => 45, 'Lard' => 35)
 );
 
 $graph = new SVGGraph(1200, 600, $settings);
