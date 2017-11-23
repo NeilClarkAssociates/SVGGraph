36ff418ed5795e59b3cf94f6264e054a30e4a3b4
David J Holland <stooge213@hotmail.com>
2017-11-23 16:29:16 +1100

    Update example

--- examples/index.php
+++ examples/index.php
@@ -6,12 +6,13 @@ $settings = array(
 );
 
 $values = array(
-  array('Dough' => 30, 'Ray' => 50, 'Me' => NULL, 'So' => 25, 'Far' => 45, 'Lard' => 35)
+  array('Dough' => 30, 'Ray' => 50, 'Me' => NULL, 'So' => 25, 'Far' => 45, 'Lard' => 35),
+  array('Dough' => 50, 'Ray' => NULL, 'Me' => 20, 'So' => 15, 'Far' => 45, 'Lard' => 65)
 );
 
 $graph = new SVGGraph(1200, 600, $settings);
 
 $graph->Values($values);
-$graph->Render('LineGraph');
+$graph->Render('MultiLineGraph');
 
 ?>
