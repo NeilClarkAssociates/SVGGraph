f5f628d32865c6bd608771f89aa999c18f5fc5d2
David J Holland <stooge213@hotmail.com>
2017-11-24 11:15:34 +1100

    :chart_with_upwards_trend: Update example

--- examples/index.php
+++ examples/index.php
@@ -2,12 +2,13 @@
 require_once('../SVGGraph.php');
 
 $settings = array(
-  'break_on_null' => true
+  'break_on_null' => true,
+  'break_minimum' => 3
 );
 
 $values = array(
-  array('Dough' => 30, 'Ray' => 50, 'Me' => NULL, 'So' => 25, 'Far' => 45, 'Lard' => 35),
-  array('Dough' => 50, 'Ray' => NULL, 'Me' => 20, 'So' => 15, 'Far' => 45, 'Lard' => 65)
+  array('Dough' => 30, 'Ray' => 50, 'Me' => NULL, 'So' => 25, 'Far' => NULL, 'Lard' => 35),
+  array('Dough' => 50, 'Ray' => NULL, 'Me' => NULL, 'So' => NULL, 'Far' => 45, 'Lard' => NULL, 'bees' => NULL, 'arse' => 78)
 );
 
 $graph = new SVGGraph(1200, 600, $settings);
